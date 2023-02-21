d87b19d83dc5739790f2507c80444b304252220e
AndrewAC21 <88219834+AndrewAC21@users.noreply.github.com>
2023-02-21 10:42:51 +0000

    inputs and outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent: string = 'https://www.w3schools.com/w3css/img_lights.jpg';
+
+  onLoaded(msg: string): void {
+    console.log('log padre', msg);
+  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
-  styleUrls: ['./img.component.scss']
+  styleUrls: ['./img.component.scss'],
 })
 export class ImgComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() imageUrl: string = '';
+  @Output() loaded = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  defaultImg: string = 'https://www.w3schools.com/howto/img_avatar.png';
+
+  ngOnInit(): void {}
+  imgLoaded(): void {
+    console.log('log hijo');
+    this.loaded.emit('loaded');
   }
-
+  imgError(): void {
+    this.imageUrl = this.defaultImg;
+  }
 }
